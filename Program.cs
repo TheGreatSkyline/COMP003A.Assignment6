026ae789f617f5835f07f12eb78e6ee5888147b1
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-02-21 18:34:50 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ 
+ */
+namespace COMP003A.Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
